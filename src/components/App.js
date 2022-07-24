edba7587e77157c607847e19822c82d7e9de475e
Alberta Lynn Brubaker <findingalberta@gmail.com>
2022-07-23 19:31:20 -0500

    commit tree 8051a09990a42ff5433bf8a44fce173d77259995

--- src/components/App.js
+++ src/components/App.js
@@ -1,1 +1,26 @@
-// create your App component here
+import React, {useState, useEffect} from "react";
+
+const App = () => {
+  const [isLoaded, setIsLoaded] = useState(false);
+  const [pupData, setPupData] = useState("")
+
+  useEffect(() => {
+    fetch('https://dog.ceo/api/breeds/image/random')
+      .then(r => r.json())
+      .then(pupper => {
+        setPupData(pupper.message);
+        setIsLoaded(isLoaded => !isLoaded)
+      }
+      )
+      .catch(e => console.error("Error:", e))
+  }, [])
+
+
+  return(
+    <div>
+      {isLoaded ? <img src={pupData} alt="A Random Dog"/> : <p>Loading...</p>}
+    </div>
+  )
+}
+
+export default App;
